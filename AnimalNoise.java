c6623c87f78cfab10ea23417713c831bd7a00e52
jrholliday94 <35750521+jrholliday94@users.noreply.github.com>
2018-04-04 14:29:20 -0500

    Update AnimalNoise.java

--- AnimalNoise.java
+++ AnimalNoise.java
@@ -1,4 +1,3 @@
-
 import model.Bird;
 import model.Cat;
 import model.Frog;
@@ -13,28 +12,31 @@ public class AnimalNoise {
 		// TODO Auto-generated method stub
 		Cat fluffy = new Cat();
 		System.out.println(fluffy.speak());
-		
+
 		TrappedZooKeeper jamantha = new TrappedZooKeeper();
 		System.out.println(jamantha.speak());
-		
+
 		Frog slimey = new Frog();
 		System.out.println(slimey.speak());
-		
+
 		Bird Pabu = new Bird();
 		System.out.println(Pabu.speak());
-		
+
 		Lion Nala = new Lion();
 		System.out.println(Nala.speak());
-		
+
 		Fox alexander = new Fox();
 		System.out.println(alexander.speak());
-		
+
 		Goat Phil = new Goat();
 		System.out.println(Phil.speak());
-		
+
 		Dog Zoe = new Dog();
 		System.out.println(Zoe.speak());
 		
+		Cow biggy = new Cow();
+		System.out.println(biggy.speak());
+
 	}
 
 }
